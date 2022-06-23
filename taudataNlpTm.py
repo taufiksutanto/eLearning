4e486f29420e004ff1ba716b7fa8dcb91b2e720f
taudata <57301176+taudataid@users.noreply.github.com>
2022-06-23 12:19:27 +0400

    dfgwr

--- taudataNlpTm.py
+++ taudataNlpTm.py
@@ -25,12 +25,11 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 #from scipy import special
 from collections import Counter
 #import matplotlib.pyplot as plt
-import networkx as nx
 import numpy as np
 import os
 import re
 import itertools
-import nltk
+import spacy
 import pandas as pd
 from nltk.corpus import wordnet as wn
 from nltk.stem import PorterStemmer;ps = PorterStemmer()
@@ -38,7 +37,8 @@ from itertools import chain
 from html import unescape
 from nltk import sent_tokenize
 from unidecode import unidecode
-#from scipy.sparse import csr_matrix
+nlp_en = spacy.load("en_core_web_sm")
+lemma_id = StemmerFactory().create_stemmer()
 
 def print_Topics(model, feature_names, Top_Topics, n_top_words):
     for topic_idx, topic in enumerate(model.components_[:Top_Topics]):
@@ -64,9 +64,7 @@ def LoadStopWords(lang='en'):
         stops = set(); lemmatizer = None
     return stops, lemmatizer
 
-def cleanText(T, fix={}, lemma=None, stops = set(), symbols_remove = True, min_charLen = 2, fixTag= True):
-    # lang & stopS only 2 options : 'en' atau 'id'
-    # symbols ASCII atau alnum
+def cleanText(T, fix={}, onlyChar=True, lemma=False, lan='id', stops = set(), symbols_remove = True, min_charLen = 2, max_charLen = 15, fixTag= True):
     pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
     t = re.sub(pattern,' ',T) #remove urls if any
     pattern = re.compile(r'ftp[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
@@ -81,30 +79,30 @@ def cleanText(T, fix={}, lemma=None, stops = set(), symbols_remove = True, min_c
     t = sent_tokenize(t) # sentence segmentation. String to list
     for i, K in enumerate(t):
         if symbols_remove:
-            K = re.sub(r'[^.,_a-zA-Z0-9 \.]',' ',K)
-        if lemma:
-            listKata = lemma(K)
-        else:
-            listKata = TextBlob(K).words
-        cleanList = []
-        for token in listKata:
-            if lemma:
-                if str(token.text) in fix.keys():
-                    token = fix[str(token.text)]
-                try:
-                    token = token.lemma_
-                except:
-                    token = lemma(token)[0].lemma_
-            else:
+            listKata = re.sub(r'[^.,_a-zA-Z0-9 -\.]',' ',K)
+
+        listKata = TextBlob(listKata).words
+        if fix:
+            for j, token in enumerate(listKata):
                 if str(token) in fix.keys():
-                    token = fix[str(token)]
-            if stops:
-                if len(token)>=min_charLen and token not in stops:
-                    cleanList.append(token)
-            else:
-                if len(token)>=min_charLen:
-                    cleanList.append(token)
-        t[i] = ' '.join(cleanList)
+                    listKata[j] = fix[str(token)]
+
+        if onlyChar:
+            listKata = [tok for tok in listKata if sum([1 for d in tok if d.isdigit()])==0]
+
+        if stops:
+            listKata = [tok for tok in listKata if str(tok) not in stops and len(str(tok))>=min_charLen]
+        else:
+            listKata = [tok for tok in listKata if len(str(tok))>=min_charLen]
+
+        if lemma and lan.lower().strip()=='id':
+            t[i] = lemma_id.stem(' '.join(listKata))
+        elif lemma and lan.lower().strip()=='en':
+            listKata = [str(tok.lemma_) for tok in nlp_en(' '.join(listKata))]
+            t[i] = ' '.join(listKata)
+        else:
+            t[i] = ' '.join(listKata)
+
     return ' '.join(t) # Return kalimat lagi
 
 def crawlFiles(dPath,types=None): # dPath ='C:/Temp/', types = 'pdf'
@@ -337,68 +335,3 @@ def lDistance(firstString, secondString):
         distances = newDistances
     return distances[-1]
 
-def kataKunci(text):
-    #tokenize the text using nltk
-    wordTokens = nltk.word_tokenize(text)
-    #assign POS tags to the words in the text
-    tagged = nltk.pos_tag(wordTokens)
-    textlist = [x[0] for x in tagged]
-
-    tagged = filter_for_tags(tagged)
-    tagged = normalize(tagged)
-
-    unique_word_set = unique_everseen([x[0] for x in tagged])
-    word_set_list = list(unique_word_set)
-
-   #this will be used to determine adjacent words in order to construct keyphrases with two words
-
-    graph = buildGraph(word_set_list)
-    #pageRank - initial value of 1.0, error tolerance of 0,0001,
-    calculated_page_rank = nx.pagerank(graph, weight='weight')
-    #most important words in ascending order of importance
-    keyphrases = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
-    #the number of keyphrases returned will be relative to the size of the text (a third of the number of vertices)
-    aThird = len(word_set_list) / 3
-    keyphrases = keyphrases[0:aThird+1]
-
-    #take keyphrases with multiple words into consideration as done in the paper - if two words are adjacent in the text and are selected as keywords, join them
-    #together
-    modifiedKeyphrases = set([])
-    dealtWith = set([]) #keeps track of individual keywords that have been joined to form a keyphrase
-    i = 0
-    j = 1
-    while j < len(textlist):
-        firstWord = textlist[i]
-        secondWord = textlist[j]
-        if firstWord in keyphrases and secondWord in keyphrases:
-            keyphrase = firstWord + ' ' + secondWord
-            modifiedKeyphrases.add(keyphrase)
-            dealtWith.add(firstWord)
-            dealtWith.add(secondWord)
-        else:
-            if firstWord in keyphrases and firstWord not in dealtWith:
-                modifiedKeyphrases.add(firstWord)
-
-            #if this is the last word in the text, and it is a keyword,
-            #it definitely has no chance of being a keyphrase at this point
-            if j == len(textlist)-1 and secondWord in keyphrases and secondWord not in dealtWith:
-                modifiedKeyphrases.add(secondWord)
-
-        i = i + 1
-        j = j + 1
-
-    return modifiedKeyphrases
-
-def Rangkum(text,M):
-    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-    sentenceTokens = sent_detector.tokenize(text.strip())
-    graph = buildGraph(sentenceTokens)
-    calculated_page_rank = nx.pagerank(graph, weight='weight')
-    #most important sentences in ascending order of importance
-    sentences = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
-    #return a 100 word summary
-    summary = ' '.join(sentences[:M])
-    summaryWords = summary.split()
-    summaryWords = summaryWords[0:101]
-    summary = ' '.join(summaryWords)
-    return summary
